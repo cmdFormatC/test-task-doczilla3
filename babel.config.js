13f2f0200df31d45931f88be5317c34646ac2966
cmdFormatC <bragin.fedor@gmail.com>
2024-02-07 12:52:52 +0300

    init commit

---
+++ babel.config.js
@@ -0,0 +1,15 @@
+const presets = [
+    ['@babel/preset-env', {
+      targets: { 
+        ie: '11',
+        firefox: '50',
+        chrome: '64',
+        safari: '11.1'
+      },
+  
+
+      useBuiltIns: "entry"
+    }]
+  ];
+  
+  module.exports = { presets }; 
