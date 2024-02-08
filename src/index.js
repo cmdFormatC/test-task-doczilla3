26b4768b12d773fb2d5402828342113ae24a42c2
cmdFormatC <117574451+cmdFormatC@users.noreply.github.com>
2024-02-08 15:58:56 +0300

    [+]add task sort

--- src/index.js
+++ src/index.js
@@ -5,9 +5,16 @@ import 'normalize.css';
 import './index.css';
 
 const taskContainer = document.querySelector('[data-tasks]');
+const directionSortButton = document.querySelector('[data-sort-direction]');
+const directionSortIcon = document.querySelector('[data-sort-icon]');
 
-let limit = 10;
-let offset = 0;
+const appState = {
+  isSortIncreasing: false,
+  limit: 10,
+  offset: 0,
+  isLoading: false,
+  tasks: [],
+}
 
 const api = new Api({
   baseUrl: 'https://todo.doczilla.pro',
@@ -27,14 +34,34 @@ function createTask(item) {
   taskContainer.append(task.generateTask())
 };
 
-function loadTasks() {
-  api.getTasks(limit, offset)
+function mountTasks() {
+  appState.tasks.forEach(element => {
+    createTask(element);
+  });
+}
+
+function unMountTasks() {
+  while (taskContainer.firstChild) {
+    taskContainer.removeChild(taskContainer.firstChild);
+  };
+}
+
+function sortTasks() {
+  appState.tasks.sort((a, b) => {
+    const dateA = new Date(a.date);
+    const dateB = new Date(b.date);
+    return appState.isSortIncreasing ? dateA - dateB : dateB - dateA;
+  });
+}
+
+const loadTasks = () => {
+  api.getTasks(appState.limit, appState.offset)
   .then((result) => {
-    console.log(result)
-    result.forEach(element => {
-      createTask(element);
-    });
-    offset += limit;
+    appState.tasks.push(...result);
+    sortTasks();
+    unMountTasks();
+    mountTasks();
+    appState.offset += appState.limit;
   })
 };
 
@@ -42,6 +69,15 @@ taskContainer.addEventListener('scroll', () => {
   if (taskContainer.scrollTop + taskContainer.clientHeight >= taskContainer.scrollHeight) {
     loadTasks();
   }
-})
+});
+
+directionSortButton.addEventListener('click', () => {
+  directionSortIcon.classList.toggle('sort__direction-arrow_reverse');
+  appState.isSortIncreasing = !appState.isSortIncreasing;
+  sortTasks();
+  unMountTasks();
+  mountTasks();
+});
+
 
 loadTasks();
